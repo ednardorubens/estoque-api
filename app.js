460f3eabdbb004cce26a95425aed4c79e17cbf3c
EDNARDO Rubens Matos Ignácio D001114 <D001114@dreads.bnb>
2018-01-31 18:35:06 -0300

    favicon

--- app.js
+++ app.js
@@ -37,32 +37,9 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger(':method :url :status :res[content-length]kb :response-time ms | [:date :remote-user :remote-addr]'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('[:method] :url :status :res[content-length] :response-time ms [:date :remote-user :remote-addr]'));
 
-const format = (tokens, req, res) => {
-  const status = res.statusCode;
-  
-  const colorStatus = status >= 500 ? 31 // red
-    : status >= 400 ? 33 // yellow
-    : status >= 300 ? 36 // cyan
-    : status >= 200 ? 32 // green
-    : 0 // no color
-
-  const ms = (res._startAt[0] - req._startAt[0]) * 1e3 +
-    (res._startAt[1] - req._startAt[1]) * 1e-6
-
-  const colorResponseTime = ms >= 3000 ? 31 // red
-    : ms >= 2000 ? 33 // yellow
-    : ms >= 1000 ? 34 // blue
-    : 32 // green
-
-  var fn = logger.compile('\x1b[42m\x1b[30m[:method]\x1b[0m \x1b[35m:url \x1b[' + colorStatus + 'm:status \x1b[35m:res[content-length] \x1b[' + colorResponseTime + 'm:response-time ms \x1b[37m[:date :remote-user :remote-addr]\x1b[0m');
-  
-  return fn(tokens, req, res);
-};
-
-app.use(logger(format));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
